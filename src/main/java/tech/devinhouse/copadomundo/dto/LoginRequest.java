a82f8e4b27c5601fd258ab2c62bb108e7974349f
Thiago de Santana <113400543+DevThiagoSantana@users.noreply.github.com>
2022-11-25 17:53:32 -0300

    Exercicio 3 semana 11

---
+++ src/main/java/tech/devinhouse/copadomundo/dto/LoginRequest.java
@@ -0,0 +1,15 @@
+package tech.devinhouse.copadomundo.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+public class LoginRequest {
+
+    @NotEmpty(message = "campo.obrigatorio")
+    private String email;
+
+    @NotEmpty(message = "campo.obrigatorio")
+    private String senha;
+}
---
+++ src/main/java/tech/devinhouse/copadomundo/dto/LoginResponse.java
@@ -0,0 +1,15 @@
+package tech.devinhouse.copadomundo.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class LoginResponse {
+
+    private String token;
+
+}
